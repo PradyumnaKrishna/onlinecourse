c1f5e0027600f2ecabb379494e201eada2ba9134
Pradyumna Krishna <51270439+PradyumnaKrishna@users.noreply.github.com>
2021-07-08 22:13:31 +0530

    add models to admin

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from .models import Course, Lesson, Instructor, Learner, Question, Choice
 
-# Register your models here.
+
+# Inlines for Admin
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    extra = 5
+
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+
+
+# Admin Format
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [LessonInline, QuestionInline]
+    list_display = ('name', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['name', 'description']
+
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+
+# Register Models to Admin
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Instructor)
+admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
